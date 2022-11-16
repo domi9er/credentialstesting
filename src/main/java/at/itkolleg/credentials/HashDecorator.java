9899fbfb15a9fce187284dd787a8140a23afe0d3
manpayer <manuel.payer96@gmx.at>
2022-11-16 09:28:27 +0100

    Zwischendokumentation

--- src/main/java/at/itkolleg/credentials/HashDecorator.java
+++ src/main/java/at/itkolleg/credentials/HashDecorator.java
@@ -1,20 +1,32 @@
 package at.itkolleg.credentials;
 
+import com.google.common.hash.Hashing;
+
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+
 
 public class HashDecorator implements ExportCredentials{
 
     protected Credentials credentials;
 
+    /**
+     * Konstruktor, der ein Objekt vom Typ Credentials erstellt und der mitgegebene Wert wird auf die globale Variable gesetzt
+     * @param credentials
+     */
     public HashDecorator(Credentials credentials)
     {
         this.credentials = credentials;
     }
 
+    /**
+     * Methode, die einen String benötigt und diesen String dann durch eine Hash Funktion in einen Hash umwandelt
+     * @param originalString
+     * @return
+     */
     public String stringHash(String originalString)
     {
-        Object Hashing = null;
+
         String sha256hex = Hashing.sha256()
                 .hashString(originalString, StandardCharsets.UTF_8)
                 .toString();
@@ -22,7 +34,11 @@ public class HashDecorator implements ExportCredentials{
         return sha256hex;
     }
 
-
+    /**
+     * Überschriebene Methode, die vom Interface ExportCredentials kommt. Der Array wird durch iteriert und die einzelnen
+     * Getter Methoden werden an der ArrayList aufgerufen. Die Iterierung endet, wenn der letzte Eintrag der Liste erreicht wurde.
+     * @param credentialsList
+     */
     @Override
     public void export(List<Credentials> credentialsList) {
         String exportString = "";
