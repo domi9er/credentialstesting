e2dbb4d606761b16c16fc563407fe3fb059b8536
manpayer <manuel.payer96@gmx.at>
2022-11-16 09:10:17 +0100

    Änderung an der HashDecorator Klasse

--- src/main/java/at/itkolleg/credentials/HashDecorator.java
+++ src/main/java/at/itkolleg/credentials/HashDecorator.java
@@ -1,9 +1,28 @@
 package at.itkolleg.credentials;
 
-public class HashDecorator {
+import java.util.List;
+
+public class HashDecorator implements ExportCredentials{
+
+    protected Credentials credentials;
 
     public HashDecorator(Credentials credentials)
     {
+        this.credentials = credentials;
+    }
+
+    public String stringHash(String originalString)
+    {
+        String sha256hex = Hashing.sha256()
+                .hashString(originalString, StandardCharsets.UTF_8)
+                .toString();
+
+        return String;
+    }
+
+
+    @Override
+    public void export(List<Credentials> credentialsList) {
 
     }
 }
