00ae2ceb8e77c280bf7bf861d6e3c7816aec8f29
manpayer <manuel.payer96@gmx.at>
2022-11-16 09:16:11 +0100

    Änderung der export Methode

--- src/main/java/at/itkolleg/credentials/HashDecorator.java
+++ src/main/java/at/itkolleg/credentials/HashDecorator.java
@@ -1,5 +1,6 @@
 package at.itkolleg.credentials;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class HashDecorator implements ExportCredentials{
@@ -13,16 +14,27 @@ public class HashDecorator implements ExportCredentials{
 
     public String stringHash(String originalString)
     {
+        Object Hashing = null;
         String sha256hex = Hashing.sha256()
                 .hashString(originalString, StandardCharsets.UTF_8)
                 .toString();
 
-        return String;
+        return sha256hex;
     }
 
 
     @Override
     public void export(List<Credentials> credentialsList) {
-
+        String exportString = "";
+        for(int i = 0;i<credentialsList.size();i++)
+        {
+            Credentials credentials = credentialsList.get(i);
+            exportString += credentials.getHost() +";"+ credentials.getUser() +";"+ stringHash(credentials.getPwd());
+            if(i < credentialsList.size()-1)
+            {
+                exportString += "\n";
+            }
+        }
+        System.out.println(exportString);
     }
 }
